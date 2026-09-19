26dbe2d5a7d93adecae5a42d707b596c64674780
liangbinjie <mailandresliang@gmail.com>
2026-09-18 18:46:15 -0600

    Enhance Astro configuration and components by adding asset handling, updating deployment workflow, and improving background image management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://liangbinjie.github.io',
     base: '/liangbinjie',
-  });
+    trailingSlash: 'always',
+});
